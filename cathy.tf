4c94b4253e86e54daaf7c5cbf701603ed03df671
u297981 <cathywang2017@users.noreply.github.com>
2020-01-02 09:44:50 -0600

    update cathy.tf

--- cathy.tf
+++ cathy.tf
@@ -1,8 +1,26 @@
-resource "aws_instance" "cathy"{
-    ami = "ami-5b41123e"
+resource "aws_instance" "HelloWorld"{
+    ami = "ami-045f5e9c2833c8631"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
 
     tags = {
-        Name = "terraform-cathy"
+        Name = "terraform-helloworld"
+    }
+}
+
+resource "aws_security_group" "instance"{
+    name = "terraform-helloworld-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
